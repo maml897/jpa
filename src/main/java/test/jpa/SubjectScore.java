7c0a59be2f8c5a3987b79eb73d27374ddafc4be8
maml <maml897@163.com>
2018-08-20 10:04:16 +0800

    ok

--- src/main/java/test/jpa/SubjectScore.java
+++ src/main/java/test/jpa/SubjectScore.java
@@ -1,6 +1,7 @@
 package test.jpa;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -19,14 +20,25 @@ public class SubjectScore {
 		
 		long s =System.currentTimeMillis();
 		
-		Query query2 = JpaUtils.createNativeQuery("select ns.ID,ns.Score,nss.YsScore from n_nsstudentsubject nss join n_nsstudent ns on nss.NsStudentID=ns.ID and ns.nsID=" + nsID +" and nss.subjectID="+subjectID +" and nss.JoinOption=1 and nss.YsScore>0", HashMap.class);
+		Query query2 = JpaUtils.createNativeQuery("select ns.ID,ns.Score,nss.YsScore from n_nsstudentsubject nss join n_nsstudent ns on nss.NsStudentID=ns.ID and ns.nsID=" + nsID +" and nss.subjectID="+subjectID +" and nss.JoinOption=1 and nss.YsScore>0 order by nss.YsScore desc", HashMap.class);
 		List<Map<String,Object>> nsStudentSubjects = query2.getResultList();
 		
 		System.out.println(nsStudentSubjects.size());
-		Map<Float, Long> group2 = nsStudentSubjects.stream().collect(Collectors.groupingBy(t -> (float)t.get("YsScore"), Collectors.counting()));
+		Map<Float, Long> group2 = nsStudentSubjects.stream().collect(Collectors.groupingBy(t -> (float)t.get("YsScore"), LinkedHashMap::new, Collectors.counting()));
 		
-		System.out.println(group2);
-		System.out.println(group2.get(1.5f));
+//		System.out.println(group2);
+//		System.out.println(group2.get(1.5f));
+		
+		long lastOrder =1;
+		long lastCount = 0;
+		
+		for(float ysScore:group2.keySet()){
+			
+			long order = lastOrder+lastCount;
+			System.out.println(ysScore+"=="+group2.get(ysScore)+"==="+order);
+			lastOrder = order;
+			lastCount = group2.get(ysScore);
+		}
 		
 		System.out.println(System.currentTimeMillis()-s);
 	}
